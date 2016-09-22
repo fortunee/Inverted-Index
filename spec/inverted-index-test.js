9f4722db8c5aa2a5bde4501c20ca0b1e4dca8dfc
fortune <legalmody@gmail.com>
2016-09-22 07:42:31 +0100

    Setting expectations and writing test suites

---
+++ spec/inverted-index-test.js
@@ -0,0 +1,59 @@
+ var index = require(./../src/js/inverted-index.js);
+
+describe("Iverted Index Object", function () {
+  describe("Read book data", function () {
+    it("should read json data", function () {
+        expect()
+    });
+
+    it("should not be empty", function () {
+        expect();
+    });
+  });
+
+  describe("Populate Index", function () {
+    it("should populate the index", function () {
+      expect();
+    });
+
+    it("should verify that index is created", function () {
+      expect();
+    });
+
+    it("should verify that keys are mapped to the correct docs", function () {
+      expect();
+    });
+  });
+
+
+  describe("Search index", function () {
+    it("should return an array of indices of the documents", function () {
+      expect();
+    });
+
+
+  });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  \\\
+
+
+
+
+
+
+});
