db9c6670ebf1879bad0bf1d973f0f27e15bac1b3
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-09-23 11:14:40 +0100

    Added a tokenize method to cleanse strings

--- src/js/inverted-index.js
+++ src/js/inverted-index.js
@@ -13,4 +13,12 @@ var Index = function() {
 
   // This will contain the search results
   this.searchResults = {};
+
+  /*
+   * Returns an array of terms from the given strings
+   * @param{String} str - String to be tokonized
+   */
+  this.tokonize = function(str) {
+    return str.replace(/[.,\/#!$%\^&\*;:'{}=\-_`~()]/g, '').trim().toLowerCase().split(' ');
+  };
 };
