8dbf46632ce272cc329da8a9dfebfc83559bd1a5
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-09-23 13:18:22 +0100

    Create index method implemented

--- src/js/inverted-index.js
+++ src/js/inverted-index.js
@@ -21,4 +21,43 @@ var Index = function() {
   this.tokonize = function(str) {
     return str.replace(/[.,\/#!$%\^&\*;:'{}=\-_`~()]/g, '').trim().toLowerCase().split(' ');
   };
+
+  /*
+   * Creates the index of a JSON file and updates it in the indexedFiles
+   * @param{String} fileName - The name of the JSON file that is to be created
+   * @param{String} fileContents - The contents of the JSON file that is to be created
+   */
+  this.createIndex = function(fileName, fileContents) {
+
+    // This will contain the indexed file contents
+    var indexedFileContents = {};
+
+    fileContents.forEach(function(item, indexNum) {
+
+      var titleTokens = this.tokonize(item.title);
+      var textTokens = this.tokenize(item.text);
+
+      // Merged array of both titleTokens and textTokens
+      var tokens = titleTokens.concat(textTokens);
+
+      // FIlter Merged array to order and get unique words
+      tokens = tokens.filter(function(token, pos) {
+        return tokens.indexOf(token) === pos;
+      });
+
+      // Set each token as a property indexedFileContents with array value
+      // of index number where they appear.
+      tokens.forEach(function(token) {
+        if (!indexedFileContents.hasOwnProperty(token)) {
+          indexedFileContents[token] = [indexNum];
+        } else {
+          indexedFileContents[token].push(indexNum);
+        }
+      });
+    });
+
+    // Update the indexed files records
+    this.indexedFiles[fileName] = indexedFileContents;
+  };
+
 };
