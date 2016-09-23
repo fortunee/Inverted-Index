74c0c1288081fc47a5d8d2cb7d7baa5beee5d1e1
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-09-23 09:10:24 +0100

    Refactored inverted-index test cases

--- spec/inverted-index-test.js
+++ spec/inverted-index-test.js
@@ -1,40 +1,55 @@
 'use strict';
-var books =
+
+var books = [
+  {
+    "title": "Alice in Wonderland",
+    "text": "Alice falls into a rabbit hole and enters a world full of imagination."
+  },
+
+  {
+    "title": "The Lord of the Rings: The Fellowship of the Ring.",
+    "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
+  }
+];
+
 describe("Iverted Index Object", function () {
+  var index = new Index();
 
   describe("Read book data", function () {
 
     it("should not be empty", function () {
-        expect(index.jsonFile.length > 0).toBeTruthy();
+        expect(index.tokenizedFiles.length > 0).toBeTruthy();
     });
   });
 
   describe("Populate Index", function () {
 
     beforeEach(function () {
-      index.createIndex(docs);
+      index.createIndex('books.json', books);
     });
 
     it("should populate the index", function () {
-
-      index.createIndex(docs);
-      expect(index.tokenizedFiles.length === 0).not.toBeTruthy();
+      expect(index.tokenizedFiles).toContain('books.json');
     });
 
     it("should verify that index is created", function () {
-      index.createIndex(docs);
-      expect(index.tokenizedFiles).toBeTruthy();
+      expect(index.tokenizedFiles.length > 0).toBeTruthy();
     });
 
     it("should verify that keys are mapped to the correct docs", function () {
-      expect();
+      expect(index.tokenizedFiles["books.js"]["of"]).toEqual([0, 1]);
     });
   });
 
 
   describe("Search index", function () {
+    beforeEach(function () {
+      index.createIndex('books.json', books);
+      index.searchIndex('books.json', "alice in Wonderland");
+    });
+
     it("should return an array of indices of the documents", function () {
-      expect();
+      expect(this.searchedResults["books.json"]["alice"]).toEqual([0]);
     });
 
 
