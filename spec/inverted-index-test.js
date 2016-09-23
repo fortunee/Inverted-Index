b412eff91dc65f68a4126e65bed5c9a477420408
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-09-23 01:47:46 +0100

    Test that verifies that book data is not empty

--- spec/inverted-index-test.js
+++ spec/inverted-index-test.js
@@ -1,23 +1,27 @@
 'use strict';
 
 describe("Iverted Index Object", function () {
+
   describe("Read book data", function () {
-    it("should read json data", function () {
-        expect();
-    });
 
     it("should not be empty", function () {
-        expect();
+        expect(index.jsonFile.length > 0).toBeTruthy();
     });
   });
 
   describe("Populate Index", function () {
+
+    beforeEach(function () {
+      index.createIndex(docs);
+    });
+
     it("should populate the index", function () {
-      expect();
+      expect(index.tokenizedFiles.length === 0)toBeTruthy();
     });
 
     it("should verify that index is created", function () {
-      expect();
+      index.createIndex(docs);
+      expect(index.tokenizedFiles.length > 0).toBeTruthy;
     });
 
     it("should verify that keys are mapped to the correct docs", function () {
