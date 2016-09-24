6400a34c089faa7a6b0a76a25078f5825ff72f29
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-09-24 19:21:46 +0100

    Refactored test suits and added a few mock json files

--- spec/inverted-index-test.js
+++ spec/inverted-index-test.js
@@ -1,18 +1,5 @@
 "use strict";
 
-var books = [{
-    "title": "Alice in Wonderland",
-    "text": "Alice falls into a rabbit hole and enters a world full of imagination."
-  },
-
-  {
-    "title": "The Lord of the Rings: The Fellowship of the Ring.",
-    "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
-  }
-];
-
-var file = [{}];
-
 var index = new Index();
 
 describe("Iverted Index Object", function() {
@@ -36,11 +23,11 @@ describe("Iverted Index Object", function() {
     });
 
     it("should verify that index is created", function() {
-      expect(index.indexedFiles["books.json"].alice).toEqual([0]);
+      expect(index.indexedFiles["books.json"].alice).toEqual([0, 1, 2]);
     });
 
     it("should verify that keys are mapped to the correct docs", function() {
-      expect(index.indexedFiles["books.json"].of).toEqual([0, 1]);
+      expect(index.indexedFiles["books.json"].of).toEqual([0, 1, 2]);
     });
   });
 
@@ -48,13 +35,42 @@ describe("Iverted Index Object", function() {
   describe("Search index", function() {
     beforeEach(function() {
       index.createIndex("books.json", books);
+      index.searchIndex("all", "alice in Wonderland");
       index.searchFile("books.json", "alice in Wonderland");
     });
 
     it("should return an array of indices of the documents", function() {
-      expect(index.searchResults["books.json"].alice).toEqual([0]);
+      expect(index.searchResults["books.json"].alice).toEqual([0, 1, 2]);
     });
 
 
   });
 });
+
+var books = [{
+    "title": "Alice in Wonderland alice",
+    "text": "Alice Alice Alice Alice falls into a rabbit hole and enters a world full of imagination."
+  },
+
+  {
+    "title": "The Lord of the Rings: The Fellowship of the Ring.",
+    "text": "An alice unusual alliance of man, elf, dwarf dwarf, wizard and hobbit seek to destroy a powerful ring."
+  },
+
+  {
+    "title": "King of kings",
+    "text": "Jesus Christ is the King of kings and Lord of lords alice"
+  }
+];
+
+
+var files = [{
+    "title": "Hello people",
+    "text": "We Know how people would love it if they're appreciated for everything they do"
+  },
+
+  {
+    "title": "This is the begining",
+    "text": "When the awesome project kicks off a lot amazing things will begin to unveil alice"
+  }
+];
