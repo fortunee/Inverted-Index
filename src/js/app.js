d1a92e231e5dacb7337275dd37ae5d409bf4054d
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-09-25 21:01:26 +0100

    Writing a custom angular directive

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+ * Inverted Index angular module
+ */
+var invertedIndex = angular.module("invertedIndex", [])
+
+
+/*
+ * File upload custom directive which handles the uploaded file
+ */
+.directive("fileUpload", function() {
+  var directive = {};
+});
