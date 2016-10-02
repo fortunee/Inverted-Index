32068c1ff00f12d270335003f249cfb58d54bfcc
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-10-03 00:23:52 +0100

    Add inverted-index.js path to be tested alone

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
     files: [
       'node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
-      'src/js/*.js',
+      'src/js/inverted-index.js',
       'spec/*.js'
     ],
 
