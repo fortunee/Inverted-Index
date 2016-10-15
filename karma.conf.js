b173bcdd69518424c33241fd86e6eca4394e71f2
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-10-15 13:29:37 +0100

    Remove PhantomJS and Install firefox launcher for karma test runner

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration
 // Generated on Sat Sep 24 2016 22:47:29 GMT+0100 (WAT)
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -64,7 +64,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+    browsers: ['Firefox'],
 
 
     // Continuous Integration mode
